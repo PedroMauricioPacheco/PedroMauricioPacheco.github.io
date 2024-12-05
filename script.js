70b95c6201da8824a0958021dbe3b817609656ed
Pedro Mauricio Pacheco Neto <101368545+PedroMauricioPacheco@users.noreply.github.com>
2024-12-05 10:52:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 window.onload = function() {
     const dataEscolhida = new Date('2024-08-19'); // Substitua por sua data específica
     const dataAtual = new Date();
+    
+    // Zera as horas, minutos, segundos e milissegundos
+    dataEscolhida.setHours(0, 0, 0, 0);
+    dataAtual.setHours(0, 0, 0, 0);
+    
     const diffTime = dataAtual - dataEscolhida;
     const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    
     document.getElementById('resultado').textContent = `Se passaram ${diffDays} dias desde ${dataEscolhida.toLocaleDateString()} e nossas vidas mudaram para sempre.`;
 };
